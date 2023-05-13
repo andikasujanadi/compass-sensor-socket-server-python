d56d73fed1a615744eaaee9c2cbd820f2644ba7a
Andika Sujanadi <51147116+andikasujanadi@users.noreply.github.com>
2023-05-14 01:50:34 +0800

    Hadeh

---
+++ mouse_movement.py
@@ -0,0 +1,36 @@
+import pyautogui
+
+init_x=960
+init_y=540
+boundary = 200
+
+x0, y0 = init_x, init_y
+x, y = 0, 0
+
+try:
+    pyautogui.moveTo(init_x, init_y)
+    while True:
+        x1, y1 = pyautogui.position()
+        if x1 < (init_x-boundary):
+            pyautogui.moveTo(init_x+boundary, y1)
+            x -= boundary*2
+        elif x1 > (init_x+boundary):
+            pyautogui.moveTo(init_x-boundary, y1)
+            x += boundary*2
+        if y1 < (init_y-boundary):
+            pyautogui.moveTo(x1, init_y+boundary)
+            y -= boundary*2
+        elif y1 > (init_y+boundary):
+            pyautogui.moveTo(x1, init_y-boundary)
+            y += boundary*2
+
+        x += x1 - x0
+        y += y1 - y0
+
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+        
+        x0, y0 = x1, y1
+except KeyboardInterrupt:
+    print()
