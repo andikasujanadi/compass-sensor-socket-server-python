e48516229e9d127331fe2bb73077c73d65ffae01
Andika Sujanadi <51147116+andikasujanadi@users.noreply.github.com>
2023-05-19 21:02:42 +0800

    Update mouse movement

--- mouse_movement.py
+++ mouse_movement.py
@@ -1,4 +1,5 @@
 import pyautogui
+import math
 
 init_x=960
 init_y=540
@@ -6,6 +7,15 @@ boundary = 200
 
 x0, y0 = init_x, init_y
 x, y = 0, 0
+angle = 90
+
+
+angle_in_degrees = 90
+angle_in_radians = math.radians(angle_in_degrees)
+result = math.cos(angle_in_radians)
+rounded_result = round(result, 15)  # Round to 15 decimal places
+
+print(rounded_result)
 
 try:
     pyautogui.moveTo(init_x, init_y)
@@ -23,9 +33,9 @@ try:
         elif y1 > (init_y+boundary):
             pyautogui.moveTo(x1, init_y-boundary)
             y += boundary*2
-
-        x += x1 - x0
-        y += y1 - y0
+    
+        x += (x1 - x0)*round(math.cos(math.radians(angle)),15) + (y1 - y0)*round(math.sin(math.radians(angle)),15)
+        y += (y1 - y0)*round(math.cos(math.radians(angle)),15) + (x1 - x0)*round(math.sin(math.radians(angle)),15)
 
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
         print(positionStr, end='')
